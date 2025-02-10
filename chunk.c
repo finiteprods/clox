86796410f2609142aa22d7673830f0a201076581
finiteprods <7748410+finiteprods@users.noreply.github.com>
2025-02-10 01:16:56 +0100

    14.3.1 a dynamic array of instructions

---
+++ chunk.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "chunk.h"
+#include "memory.h"
+
+void initChunk(Chunk* chunk) {
+  chunk->count = 0;
+  chunk->capacity = 0;
+  chunk->code = NULL;
+}
+
+void freeChunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  initChunk(chunk);
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte) {
+  // if at capacity... shouldn't ever be > but just to be defensive
+  if (chunk->count >= chunk->capacity) {
+    int oldCapacity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCapacity);
+    chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+  }
+
+  chunk->code[chunk->count] = byte;
+  chunk->count++;
+}
