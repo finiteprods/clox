06fbf24849c4502afc509578f807481739da0959
finiteprods <7748410+finiteprods@users.noreply.github.com>
2026-07-17 12:02:58 +0200

    16.2.1 scanning tokens

--- scanner.c
+++ scanner.c
@@ -1,3 +1,6 @@
+#include <string.h>
+
+#include "common.h"
 #include "scanner.h"
 
 // scanner state
@@ -14,4 +17,39 @@ void initScanner(const char *source) {
   scanner.start = source;
   scanner.current = source;
   scanner.line = 1;
+}
+
+// check whether at end of string i.e. current char is null byte
+static bool isAtEnd() {
+  return *scanner.current == '\0';
+}
+
+// make a (bona fide) token
+static Token makeToken(TokenType ty) {
+  Token tok;
+  tok.type = ty;
+  tok.start = scanner.start;
+  tok.length = (int)(scanner.current - scanner.start);
+  tok.line = scanner.line;
+  return tok;
+}
+
+// make an error token with given error message (string literal in practice)
+static Token errorToken(const char *msg) {
+  Token tok;
+  tok.type = TOKEN_ERROR;
+  tok.start = msg;
+  tok.length = (int)strlen(msg);
+  tok.line = scanner.line;
+  return tok;
+}
+
+Token scanToken() {
+  scanner.start = scanner.current;
+  if (isAtEnd())
+    return makeToken(TOKEN_EOF);
+
+  // TODO ...
+
+  return errorToken("Unexpected character.");
 }
